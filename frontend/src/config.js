123ccf35ee8141bdb67c7f525c21e1f59693e709
dopemukesh <dope.mukeshyadav@gmail.com>
2025-04-16 03:21:48 +0530

    Update BASE_URL to use production backend URL and comment out local development options

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,10 +2,10 @@
 // export const BASE_URL = "http://localhost:3000";
 
 // If you want to use production URL in future:
-// export const BASE_URL = "https://collegeproject02.onrender.com"; // production backend URL
+export const BASE_URL = "https://collegeproject02.onrender.com"; // production backend URL
 
 
 // // conditional type 
-const BASE_URL = window.location.hostname === 'localhost'
-    ? 'http://localhost:3000' // local backend URL
-    : 'https://collegeproject02.onrender.com'; // actual backend URL aayega
+// const BASE_URL = window.location.hostname === 'localhost'
+//     ? 'http://localhost:3000' // local backend URL
+//     : 'https://collegeproject02.onrender.com'; // actual backend URL aayega
