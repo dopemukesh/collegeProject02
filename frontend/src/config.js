5cb0d37cf569fa8cd244d909b7fa3dc1740df8a1
dopemukesh <dope.mukeshyadav@gmail.com>
2025-04-16 03:03:18 +0530

    Update project metadata and configuration
    - Change project name in package.json to "college-project02"
    - Update version in package.json to "0.0.1"
    - Modify index.html title to "College Project"
    - Comment out local BASE_URL in config.js and update production URL comment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,11 +1,11 @@
 // Local Development URL
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
 
 // If you want to use production URL in future:
-// export const BASE_URL = "https://your-live-backend.com";
+// export const BASE_URL = "https://collegeproject02.onrender.com"; // production backend URL
 
 
 // // conditional type 
-// const BASE_URL = window.location.hostname === 'localhost'
-//     ? 'http://localhost:3000' // local backend URL
-//     : 'https://collegeproject-5vpp.onrender.com'; // actual backend URL aayega
+const BASE_URL = window.location.hostname === 'localhost'
+    ? 'http://localhost:3000' // local backend URL
+    : 'https://collegeproject02.onrender.com'; // actual backend URL aayega
